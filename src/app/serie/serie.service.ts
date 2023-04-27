917fed047b10a2e770060877142f9ed0f4ea1984
Juan Lopez <je.lopezu1@uniandes.edu.co>
2023-04-27 18:05:17 -0500

    cambios

--- src/app/serie/serie.service.ts
+++ src/app/serie/serie.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Serie } from './serie';
-import { environment } from '../../environments/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class SerieService {
-  private apiUrl = environment.baseUrl + '/series.json';
+  private apiUrl = environment.baseUrl;
   constructor(private http: HttpClient) { }
   getSeries(): Observable<Serie[]> {
     return this.http.get<Serie[]>(this.apiUrl);
